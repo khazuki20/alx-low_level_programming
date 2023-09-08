25cab24f0c401fb4e86d23cc91aecf748c40cd42
khazuki20 <afesojohn08@gmail.com>
2023-09-08 07:36:18 -0700

    codes that print sizes of various types of files

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * main - print out sizes of data of types in c
+ * code by Khazuki
+ * Return: 0
+*/
+int main(void)
+{
+	char a;
+	int b;
+	long int c;
+	long long int d;
+	float f;
+
+	printf("Size of a char: %lu bytes(S)\n", (unsigned long)sizeof(a));
+	printf("Size of a int: %lu bytes(S)\n", (unsigned long)sizeof(b));
+	printf("Size of a long int: %lu bytes(S)\n", (unsigned long)sizeof(c));
+	printf("Size of a long long int: %lu bytes(S)\n", (unsigned long)sizeof(d));
+	printf("Size of a float: %lu bytes(S)\n", (unsigned long)sizeof(f));
+	return (0);
+}
