8339dcdccf6f0bc365f746cf90ff94ebdba6ed80
khazuki20 <afesojohn08@gmail.com>
2023-09-11 04:53:58 -0700

    code that prints diff types of data

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,20 +1,22 @@
 #include <stdio.h>
 /**
- * main -print out sizes of data of types in c
+ * main - Entry point of the program
  *
- * Return: 0
-*/
+ * Description: This program prints the size of various data types.
+ * Return: 0 (Success)
+ */
 int main(void)
 {
 char a;
 int b;
 long int c;
 long long int d;
-float e;
-printf("Size of a char: %lu bytes(s)\n", (unsigned long)sizeof(a));
-printf("Size of a int: %lu bytes(s)\n", (unsigned long)sizeof(b));
-printf("Size of a long int: %lu bytes(s)\n", (unsigned long)sizeof(c));
-printf("Size of a long long int: %lu bytes(s)\n", (unsigned long)sizeof(d));
-printf("Size of a float: %lu bytes(s)\n", (unsigned long)sizeof(e));
+float f;
+printf("Size of a char: %lu byte(s)\n", (unsigned long)sizeof(a));
+printf("Size of an int: %lu byte(s)\n", (unsigned long)sizeof(b));
+printf("Size of a long int: %lu byte(s)\n", (unsigned long)sizeof(c));
+printf("Size of a long long int: %lu byte(s)\n", (unsigned long)sizeof(d));
+printf("Size of a float: %lu byte(s)\n", (unsigned long)sizeof(f));
+
 return (0);
 }
