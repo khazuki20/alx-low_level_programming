dc97d86fb6088bbf0daa5f671da5456f90dd26fa
khazuki20 <afesojohn08@gmail.com>
2023-09-13 18:37:47 -0700

    print putchar followed by new line

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ *main - write a function that prints the alphabet
+ *
+ *Return: return 0
+ */
+
+void print_alphabet(void)
+
+{
+	char c;
+
+	for
+	(c = 'a'; c <= 'z'; c++);
+{
+	_putchar(c);
+}
+	_putchar('\n');
+}
